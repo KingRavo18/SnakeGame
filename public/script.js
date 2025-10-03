7e087f0b470e3d190b8c871e6c81b4442d41a42d
KingRavo18 <rkingovskis14@gmail.com>
2025-10-03 23:39:21 +0300

    Created a game board and start button

--- public/script.js
+++ public/script.js
@@ -2,11 +2,47 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("start_btn").onclick = () => startGame();
 });
 
-function startGame() {
+const player_avatar = document.createElement("div");
+const game_tick = 100;
+let interval = null;
+let positionX = 0;
+let positionY = 0;
+
+function startGame(){
     document.getElementById("start_btn").style.display = "none";
-    const game_screen_width = document.getElementById("game_screen").clientWidth; 
-    const game_screen_height = document.getElementById("game_screen").clientHeight; 
+    document.getElementById("restart_btn").style.display = "none";
+    const game_screen = document.getElementById("game_screen");
+    const game_screen_width = game_screen.clientWidth; 
+    const game_screen_height = game_screen.clientHeight; 
+
+    player_avatar.classList.add("player_avatar");
+    player_avatar.style.height = `${Math.ceil(game_screen_height / 12)}px`;
+    player_avatar.style.width = `${Math.ceil(game_screen_width / 12)}px`;
+    game_screen.appendChild(player_avatar);
+    player_avatar.style.marginLeft = `${positionX}px`;
+    player_avatar.style.marginTop = `${positionY}px`;
+
+    interval = setInterval(() => avatar_move(game_screen_width, game_screen_height), game_tick);
+}
+
+function avatar_move(game_screen_width, game_screen_height){
+    positionX += Math.ceil(game_screen_width / 12 / 2);
+    if(positionX >= game_screen_width){
+        gameOver();
+    }
+    player_avatar.style.marginLeft = `${positionX}px`;
+}
+
+function gameOver(){
+    clearInterval(interval);
+    const restartButton = document.getElementById("restart_btn");
+    restartButton.style.display = "block";
+    restartButton.onclick = restartGame;
     
-    document.getElementById("game_screen").style.width = `${game_screen_width}px`;
-    document.getElementById("game_screen").style.height = `${game_screen_height}px`;
+}
+
+function restartGame(){
+    positionX = 0;
+    positionY = 0;
+    startGame();
 }
