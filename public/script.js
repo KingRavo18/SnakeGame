ddd5158f8bfc4912c73a7b5eed3d223cb604220a
KingRavo18 <rkingovskis14@gmail.com>
2025-10-04 21:04:10 +0300

    Created the game board

--- public/script.js
+++ public/script.js
@@ -2,12 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("start_btn").onclick = () => startGame();
 });
 
-const player_avatar = document.createElement("div");
+//const player_avatar = document.createElement("div");
 const start_button = document.getElementById("start_btn");
-const game_tick = 100;
-let interval = null;
-let positionX = 0;
-let positionY = 0;
+const tile_size = 40; 
+//const game_tick = 100;
+//let interval = null;
+//let positionX = 0;
+//let positionY = 0;
 
 
 function startGame(){
@@ -17,20 +18,31 @@ function startGame(){
 }
 
 function gameSetup() {
+    const tile_x_amount = 12; 
+    const tile_y_amount = 12; 
+    const total_tile_amount = tile_x_amount * tile_y_amount;
     const game_screen = document.getElementById("game_screen");
-    const game_screen_width = game_screen.clientWidth; 
-    const game_screen_height = game_screen.clientHeight; 
-    const total = Math.ceil(game_screen_height * game_screen_width / (42 ** 2));
-    console.log(total);
+    game_screen.style.width = `${tile_size * tile_x_amount + 18}px`;
+    game_screen.style.height = `${tile_size * tile_y_amount + 18}px`;
 
-    player_avatar.classList.add("player_avatar");
-    player_avatar.style.height = `${Math.ceil(game_screen_height / 12)}px`;
-    player_avatar.style.width = `${Math.ceil(game_screen_width / 12)}px`;
-    player_avatar.style.marginLeft = `${positionX}px`;
-    player_avatar.style.marginTop = `${positionY}px`;
-    game_screen.appendChild(player_avatar);
+    for(let i = 1; i < total_tile_amount; i++){
+        let grid_tile = document.createElement("div");
+        grid_tile.style.height = `${tile_size}px`;
+        grid_tile.style.width = `${tile_size}px`;
+        grid_tile.classList.add("grid_tile");
+        game_screen.appendChild(grid_tile);
+    }
+    
+
+    //player_avatar.classList.add("player_avatar");
+    //player_avatar.style.height = `${Math.ceil(game_screen_height / 12)}px`;
+    //player_avatar.style.width = `${Math.ceil(game_screen_width / 12)}px`;
+    //player_avatar.style.marginLeft = `${positionX}px`;
+    //player_avatar.style.marginTop = `${positionY}px`;
+    //game_screen.appendChild(player_avatar);
 }
 
+/*
 function avatarMove(game_screen_width, game_screen_height){
     positionX += Math.ceil(game_screen_width / 12 / 2);
     if(positionX >= game_screen_width){
@@ -52,3 +64,4 @@ function restartGame(){
     positionY = 0;
     startGame();
 }
+*/
